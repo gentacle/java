40982b47a338057186b91b6b234c06351afe1993
gentacle <gentacle00@gmail.com>
2018-12-04 16:09:34 +0900

    make some for() use printf(%d)

--- java_tutorial/src/java_tutorial_03/App.java
+++ java_tutorial/src/java_tutorial_03/App.java
@@ -23,6 +23,30 @@ public class App {
 		sb.append("I am a lion tamer");
 		
 		System.out.println(sb.toString());
+		
+		
+		
+		StringBuilder s = new StringBuilder();
+		
+		s.append("My name is ThreeMan.")
+		.append(" ")
+		.append("I am a tree safer.");
+		
+		System.out.println(s.toString());
+		
+		
+		//// Formatting //////////////////////////////////////////////////
+		
+		System.out.print("Here is some text.\tThat was a tab.\nThat was newLine.");
+		System.out.println("More text.");
+		
+		System.out.printf("Total cost %-10d; quantity is %d\n", 5, 120);
+		
+		for (int i = 0; i < 20; i++) {
+			System.out.printf("%2d: some text here\n", i);
+			
+		}
+		
 	}
 	
 
