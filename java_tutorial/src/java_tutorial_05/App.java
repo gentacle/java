13a389a464a4b36d43291b775343e9565a09b971
gentacle <gentacle00@gmail.com>
2018-12-04 17:17:52 +0900

    make classes.

---
+++ java_tutorial/src/java_tutorial_05/App.java
@@ -0,0 +1,15 @@
+package java_tutorial_05;
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/146346?start=0
+//Inheritance
+
+public class App {
+
+	public static void main(String[] args) {
+		Machine machine1 = new Machine();
+		
+		machine1.start();
+		machine1.stop();
+		
+	}
+
+}
---
+++ java_tutorial/src/java_tutorial_05/Machine.java
@@ -0,0 +1,14 @@
+package java_tutorial_05;
+
+public class Machine {
+	public void start() {
+		System.out.println("Machine started.");
+		
+	}
+	
+	public void stop() {
+		System.out.println("Machine stopped.");
+		
+	}
+
+}
