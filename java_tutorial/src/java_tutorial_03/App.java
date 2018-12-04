bae52079902d71df35e261fb2946a09a04b47a63
gentacle <gentacle00@gmail.com>
2018-12-04 15:43:55 +0900

    tutorial 3 start

---
+++ java_tutorial/src/java_tutorial_03/App.java
@@ -0,0 +1,18 @@
+package java_tutorial_03;
+
+public class App {
+	
+	public static void main(String[] args) {
+
+		
+		String info = "";
+		
+		info += "My name is gentacle.";
+		info += " ";
+		info += "I am a builder.";
+		
+		System.out.println(info);
+	}
+	
+
+}
