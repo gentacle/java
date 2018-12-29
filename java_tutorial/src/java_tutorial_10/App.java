47a8d6ebc115caecad03069b3b8809c5700f24a5
gentacle <gentacle00@gmail.com>
2018-12-29 15:13:36 +0900

    for Encapsulation..

---
+++ java_tutorial/src/java_tutorial_10/App.java
@@ -0,0 +1,29 @@
+package java_tutorial_10;
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/149507?start=570
+//Encapsulation
+
+
+class Plant{
+
+	public static final int ID = 7;
+	private String name;
+
+	//RMB >> source >> Generate Getter and Setter
+	//make get set method
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	}
+}
