33bb7442661d196dc57d195131456afe32fe1f40
gentacle <gentacle00@gmail.com>
2018-12-29 15:23:21 +0900

    Reference API Docs

--- java_tutorial/src/java_tutorial_10/App.java
+++ java_tutorial/src/java_tutorial_10/App.java
@@ -2,12 +2,24 @@ package java_tutorial_10;
 //https://www.udemy.com/java-tutorial/learn/v4/t/lecture/149507?start=570
 //Encapsulation
 
+//https://docs.oracle.com/javase/7/docs/api/java/lang/String.html#CASE_INSENSITIVE_ORDER
 
 class Plant{
 
 	public static final int ID = 7;
 	private String name;
+	
+	public String getDate() {
+		String data = "some stuff" + calculateGrowthForecast();
+		
+		return data;
+	}
+	
+	private int calculateGrowthForecast() {
+		return 9;
+	}
 
+	
 	//RMB >> source >> Generate Getter and Setter
 	//make get set method
 	public String getName() {
