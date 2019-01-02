8c3c04a0069015a506932877a6d7d6aff3b35c88
gentacle <gentacle00@gmail.com>
2019-01-02 16:41:54 +0900

    usefule upcasting.. careful downcasting..

--- java_tutorial/src/java_tutorial_12/App.java
+++ java_tutorial/src/java_tutorial_12/App.java
@@ -1,4 +1,7 @@
 package java_tutorial_12;
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/152300?start=15
+//Up/Down casting
+
 
 class Machine{
 	public void start() {
@@ -28,8 +31,30 @@ public class App {
 		camera1.start();
 		camera1.snap();
 		
+		//Upcasting 자식을 부모의 형태로 재정의.
+		//모든 인자를 물려받은 상태에서 형태변환하기때문에 가능
 		Machine machine2 = camera1;
-		machine2.start();
+		machine2.start();		
+		//machine2.snap();	error
+
 		
+		//Downcasting 본질적으로 안전하지않다.
+		//자식클래스가 부모클래스의 형태로 인터페이스를 받아 객체화됨.
+		//부모의 인자를 모두 가지고 있는 자식(부모의 모든 성질을 인터페이스로 받아 성립)
+		Machine machine3 = new Camera();
+//		Machine machine3 = (Machine)new Camera();
+//		(부모형)은 생략. 명시적으로 적어두지않으면 안전하지않다. JVM에서 유추가능한 형태로 변환하기때문.
+		
+		Camera camera2 = (Camera)machine3;
+		camera2.start();
+		camera2.snap();
+		
+		
+//		Doesn't work --- runtime error.
+		Machine machine4 = new Machine();
+//		Camera camera3 = (Camera)machine4;
+		//카메라라는 형식 안에 머신의 모든 인자가 들어있지 않다.
+//		camera3.start();
+//		camera3.snap();
 	}
 }
