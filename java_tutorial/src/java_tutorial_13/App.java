0d420bd72e4b59955e5aec1b2dba3239b3026c89
gentacle <gentacle00@gmail.com>
2019-01-02 16:46:48 +0900

    Using Generics

---
+++ java_tutorial/src/java_tutorial_13/App.java
@@ -0,0 +1,11 @@
+package java_tutorial_13;
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/152306?start=0
+//Using Generics
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	}
+}
+ 
