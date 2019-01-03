8246d1a02e08043a2b727b1e9131c50eaee892ce
gentacle <gentacle00@gmail.com>
2019-01-03 17:52:04 +0900

    add some information..

--- java_tutorial/src/java_tutorial_15/App.java
+++ java_tutorial/src/java_tutorial_15/App.java
@@ -2,11 +2,16 @@ package java_tutorial_15;
 
 //https://www.udemy.com/java-tutorial/learn/v4/t/lecture/156484?start=45
 //Anonymous Classes
-
+//익명클래스 사용방법.
+//https://yookeun.github.io/java/2017/01/24/java-anonymousclass/
 class Machine {
 	public void start() {
-		System.out.println("Starting machine...");
+		System.out.println("class Machine : 'Starting machine...'");
 	}
+}
+
+interface Plant{
+	public void grow();
 }
 
 public class App {
@@ -18,8 +23,20 @@ public class App {
 			}
 		};
 		
-		machine1.start();
+		machine1.start();  //:> Camera snapping ......
+		//따로 객체화를 하지않고 바로 오버라이드 한 뒤 실행하니 바뀐체로 실행됨.
+		//뭐지 무엇을 암시하는 것이지?
 
+
+		Plant plant1 = new Plant() {
+			
+			@Override
+			public void grow() {
+				System.out.println("Plant growing....!");
+			}
+		};
+		
+		plant1.grow();
 		
 	}
 }
