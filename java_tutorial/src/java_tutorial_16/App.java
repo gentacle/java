008126a4e096c44e36da05db6e5b3040907e7950
gentacle <gentacle00@gmail.com>
2019-01-03 18:13:41 +0900

    add throw FileNotFoundException

--- java_tutorial/src/java_tutorial_16/App.java
+++ java_tutorial/src/java_tutorial_16/App.java
@@ -1,8 +1,20 @@
 package java_tutorial_16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //https://www.udemy.com/java-tutorial/learn/v4/t/lecture/156510?start=0
 //Reading Files Using Scanner
 
 public class App {
 
+	public static void main(String[] args) throws FileNotFoundException {
+		String fileName = "C:/Users/gentacle/Documents/workspace/java/example.txt";
+		
+		File textFile = new File(fileName);
+		
+		// ERROR CODE >> Add throws declaration. 예외던지기. 안에 try catch 안쓰고 던짐.
+		Scanner in = new Scanner(textFile);
+	}
 }
