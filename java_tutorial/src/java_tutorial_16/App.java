eef6ca13406fe845d81b927edb32804c24aebb30
gentacle <gentacle00@gmail.com>
2019-01-04 19:13:07 +0900

    add nextLine()..

--- java_tutorial/src/java_tutorial_16/App.java
+++ java_tutorial/src/java_tutorial_16/App.java
@@ -17,10 +17,19 @@ public class App {
 		// ERROR CODE >> Add throws declaration. 예외던지기. 안에 try catch 안쓰고 던짐.
 		Scanner in = new Scanner(textFile);
 
+		int value = in.nextInt();
+		System.out.println("Read value: " + value);
+
+		//nextInt() 사용후 커서가 넘어가지않아서 공백줄이 생성됨.
+		//이를 제거하기 위해 nextLine()를 사용해 다음줄을 호출
+		in.nextLine();
+		
+		int count = 2;
 		while (in.hasNextLine()) {
 			String line = (String) in.nextLine();
 
-			System.out.println(line);
+			System.out.println(count + ": " +line);
+			count++;
 		}
 
 		in.close();
