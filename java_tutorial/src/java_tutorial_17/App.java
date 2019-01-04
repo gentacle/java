f4f453fd204c75b7c3a80881829b990e9c2c8197
gentacle <gentacle00@gmail.com>
2019-01-04 21:53:18 +0900

    add try/catch and some exceptions.

--- java_tutorial/src/java_tutorial_17/App.java
+++ java_tutorial/src/java_tutorial_17/App.java
@@ -11,20 +11,38 @@ public class App {
 
 	public static void main(String[] args) {
 
-		File file = new File("example_java_tutorial_17.txt");
-
-//		FileReader fileReader = new FileReader(file);
-//		error >> Surround with try/catch
-//		예외를 날리는게 아니라 안에서 try/catch로 자체예외처리함.
+//		File file = new File("example_java_tutorial_17.txt");
+//
+////		FileReader fileReader = new FileReader(file);
+////		error >> Surround with try/catch
+////		예외를 날리는게 아니라 안에서 try/catch로 자체예외처리함.
+//
+//		try {
+//			FileReader fileReader = new FileReader(file); 
+//
+//			//This will not be executed if an exception is thrown.
+//			System.out.println("Continuing . . .");
+//			
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//			System.out.println("error information >> File not found: '" + file.toString()+"'\n");
+//		}
+//		
 
 		try {
-			FileReader fileReader = new FileReader(file);
-			
-			fileReader
+			openFile();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// 이런 단순에러표시는 좀 쓸모없음.
+			System.out.println("Could not open file");
 		}
+
+		System.out.println("Finished.");
+	}
+
+	public static void openFile() throws FileNotFoundException {
+		File file = new File("test.txt");
+
+		FileReader fileReader = new FileReader(file);
 
 	}
 
