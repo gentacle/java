82edc201c590a84c56ca6015b3067821b9729446
gentacle <gentacle00@gmail.com>
2019-01-09 17:34:47 +0900

    start 'Runtime vs. Checked Exceptions'.

---
+++ java_tutorial/src/java_tutorial_19/App.java
@@ -0,0 +1,12 @@
+package java_tutorial_19;
+
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/212825?start=0
+//Runtime vs. Checked Exceptions
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
