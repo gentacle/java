9c0124dc593f6e489cebca6448116f218cb111dd
gentacle <gentacle00@gmail.com>
2019-01-10 15:24:07 +0900

    about 'Inner Classes'..

--- java_tutorial/src/java_tutorial_25/App.java
+++ java_tutorial/src/java_tutorial_25/App.java
@@ -1,10 +1,19 @@
 package java_tutorial_25;
 
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/309536?start=0
+//Inner Classes
+
 public class App {
 
 	public static void main(String[] args) {
 		
 		Robot robot = new Robot(5);
 		robot.start();
+		
+//		Robot.Brain brain = robot.new Brain();
+//		brain.think();
+		
+		Robot.Battery battery = new Robot.Battery();
+		battery.charge();
 	}
 }
--- java_tutorial/src/java_tutorial_25/Robot.java
+++ java_tutorial/src/java_tutorial_25/Robot.java
@@ -3,23 +3,40 @@ package java_tutorial_25;
 public class Robot {
 
 	private int id;
-	
-	private class Brain{
+
+	private class Brain {
 		public void think() {
-			System.out.println("Robot "+id+" is thinking.");
+			System.out.println("Robot " + id + " is thinking.");
+		}
+	}
+
+	static class Battery {
+		public void charge() {
+			System.out.println("Battery charging...");
 		}
 	}
 
 	public Robot(int id) {
 		this.id = id;
 	}
-	
 
 	public void start() {
-		System.out.println("Starting Robot for id '"+id+"'");
+		System.out.println("Starting Robot for id '" + id + "'");
 
 		Brain brain = new Brain();
 		brain.think();
+
+		String name = "gentacle";
+
+		class Temp {
+			public void doSometing() {
+				System.out.println("ID is " + id);
+				System.out.println("My name is " + name);
+			}
+		}
+		
+		 Temp temp = new Temp();
+		 temp.doSometing();
 	}
-	
+
 }
