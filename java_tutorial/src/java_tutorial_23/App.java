0bf0f31972f4532dd24f46f84d2fc6dcf91d59b2
gentacle <gentacle00@gmail.com>
2019-01-10 14:23:39 +0900

    add .write and newline()

--- java_tutorial/src/java_tutorial_23/App.java
+++ java_tutorial/src/java_tutorial_23/App.java
@@ -1,12 +1,13 @@
 package java_tutorial_23;
 
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/256668?start=0
+//Creating and Writing Text Files
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/256668?start=0
-//Creating and Writing Text Files
 
 public class App {
 
@@ -15,10 +16,14 @@ public class App {
 		File file = new File("test.txt");
 		
 		try(BufferedWriter br = new BufferedWriter(new FileWriter(file))) {
+			br.write("this line one");
+			br.newLine();
+			br.write("this line two");
+			br.newLine();
+			br.write("last line");
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("can't open file "+file.toString());
 		}
 	}
 }
