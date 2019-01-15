ddb7bedfdd52fde65113902f2fd10c319beb7a2a
gentacle <gentacle00@gmail.com>
2019-01-15 21:29:14 +0900

    copy jt28

---
+++ java_tutorial/src/java_tutorial_29/Person.java
@@ -0,0 +1,24 @@
+package java_tutorial_29;
+
+import java.io.Serializable;
+
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/424118?start=509
+//Serializing Arrays
+
+public class Person implements Serializable{
+	
+	private static final long serialVersionUID = 887769536657506509L;
+	private int id;
+	private String name;
+
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+
+}
