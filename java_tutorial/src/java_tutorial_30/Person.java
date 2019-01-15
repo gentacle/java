727ed2ea05300b4afe75d8e0f94ea8cd67030890
gentacle <gentacle00@gmail.com>
2019-01-15 22:44:04 +0900

    change id -> private 'transient' int id;

--- java_tutorial/src/java_tutorial_30/Person.java
+++ java_tutorial/src/java_tutorial_30/Person.java
@@ -2,20 +2,20 @@ package java_tutorial_30;
 
 import java.io.Serializable;
 
-//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/424118?start=509
-//Serializing Arrays
+//https://www.udemy.com/java-tutorial/learn/v4/t/lecture/807408?start=0
+//The Transient Keyword and More Serialization
 
-public class Person implements Serializable{
-	
+public class Person implements Serializable {
+
 	private static final long serialVersionUID = 887769536657506509L;
-	private int id;
+	private transient int id;
 	private String name;
 
 	public Person(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Person [id=" + id + ", name=" + name + "]";
--- java_tutorial/src/java_tutorial_30/WriteObjects.java
+++ java_tutorial/src/java_tutorial_30/WriteObjects.java
@@ -1,11 +1,9 @@
 package java_tutorial_30;
 
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class WriteObjects {
 
@@ -13,21 +11,9 @@ public class WriteObjects {
 		
 		System.out.println("Writing objects...");
 		
-		Person[] people = {new Person(1, "Sue"), new Person(99, "mike"), new Person(7, "Rori")};
-		
-		ArrayList<Person> peopleList = new ArrayList<>(Arrays.asList(people));
-		
 		try(FileOutputStream fs = new FileOutputStream("test.ser"); ObjectOutputStream os = new ObjectOutputStream(fs)){
-						
-			os.writeObject(people);
-			
-			os.writeObject(peopleList);
-			
-			os.writeInt(peopleList.size());
-			
-			for(Person person:peopleList) {
-				os.writeObject(person);
-			}
+			Person person = new Person(7, "Bob");
+			os.writeObject(person);
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
